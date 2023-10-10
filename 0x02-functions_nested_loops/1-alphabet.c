19938532e219ca4902f10707bf1e14a51565ef9e
Iszzytech <Iszzytechofficial@gmail.com>
2023-10-10 00:32:43 -0700

    Updated

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this function prints alphabetic
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
