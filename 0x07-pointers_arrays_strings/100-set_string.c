69e50a00173218c82e1784d5100aa2a06973795f
Iszzytech <Iszzytechofficial@gmail.com>
2023-10-24 23:36:05 -0700

    Updated

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * set_string - main function
+  *
+  * @s: Function param
+  *
+  * @to: Function param
+  *
+  * Return: Always 0.
+  */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
+
